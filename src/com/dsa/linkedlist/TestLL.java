97b16c12f08deb28ef2cee178d2db42559839507
Chethan Kumar S D <Chethan Kumar S D@TYSS-Chethan-Kumar>
2024-04-07 12:33:57 +0530

    clear() added

--- src/com/dsa/linkedlist/TestLL.java
+++ src/com/dsa/linkedlist/TestLL.java
@@ -2,22 +2,14 @@ package com.dsa.linkedlist;
 
 import java.util.LinkedList;
 
-class Node<E> {
-	E data;
-	Node<E> next;
-
-	public Node(E data) {
-		this.data = data;
-	}
-
-	@Override
-	public String toString() {
-		return data + "";
-	}
-}
-
 class LL<E> {
+
 	Node<E> head;
+	private static int size;
+
+	public int size() {
+		return size;
+	}
 
 	public void addFirst(E data) {
 		Node<E> newNode = new Node<>(data);
@@ -27,6 +19,7 @@ class LL<E> {
 			newNode.next = head;
 			head = newNode;
 		}
+		size++;
 	}
 
 	public void addLast(E data) {
@@ -40,6 +33,7 @@ class LL<E> {
 			}
 			node.next = newNode;
 		}
+		size++;
 	}
 
 	public String toString() {
@@ -61,7 +55,7 @@ class LL<E> {
 	}
 
 	public boolean contains(E data) {
-		return indexOf(data) >= 0 ;
+		return indexOf(data) >= 0;
 	}
 
 	public boolean remove(E data) {
@@ -70,9 +64,11 @@ class LL<E> {
 		}
 		if (head.data != null && head.data.equals(data)) {
 			head = head.next;
+			size--;
 			return true;
 		} else if (data == null && head.data == data) {
 			head = head.next;
+			size--;
 			return true;
 		}
 		for (Node<E> newNode = head,
@@ -80,32 +76,64 @@ class LL<E> {
 			if (newNode.data != null && newNode.data.equals(data)) {
 				previousNode.next = newNode.next;
 				newNode.next = null;
+				size--;
 				return true;
 			} else if (data == null && newNode.data == data) {
 				previousNode.next = newNode.next;
 				newNode.next = null;
+				size--;
 				return true;
 			}
 
 		}
 		return false;
 	}
+
 	public int indexOf(E data) {
 		if (head == null) {
 			return -1;
 		}
-		int index = 0 ;
+		int index = 0;
 		for (Node<E> newNode = head; newNode != null; newNode = newNode.next) {
 			if (newNode.data != null && newNode.data.equals(data)) {
 				return index;
 			} else if (data == null && newNode.data == data) {
 				return index;
 			}
-			index++ ;
+			index++;
 		}
 		return -1;
 	}
-	
+
+	public void clear() {
+		if (head != null) {
+			Node<E> newNode;
+			while (head.next != null) {
+				newNode = head.next;
+				head.data = null;
+				head.next = null;
+				head = newNode;
+				size--;
+			}
+			head = null;
+			size--;
+		}
+	}
+
+	private static class Node<E> {
+		E data;
+		Node<E> next;
+
+		public Node(E data) {
+			this.data = data;
+		}
+
+		@Override
+		public String toString() {
+			return data + "";
+		}
+	}
+
 }
 
 public class TestLL {
@@ -120,8 +148,18 @@ public class TestLL {
 		ll.addLast(null);
 
 		System.out.println(ll);
-		
 		System.out.println(ll.indexOf(null));
+		System.out.println(ll.size());
+		System.out.println("===========================");
+
+		System.out.println("Removed ==> " + ll.remove(40));
+		System.out.println(ll);
+		System.out.println(ll.size());
+		System.out.println("===========================");
+
+		ll.clear();
+		System.out.println(ll);
+		System.out.println(ll.size());
 
 		LinkedList<Integer> l = new LinkedList<Integer>();
 		l.contains(10);
